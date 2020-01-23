6f9ba8a9ff51b540a8cd0815768d34094900b636
pmg4 <117430404@umail.ucc.ie>
2020-01-23 15:36:27 +0000

    Added a basic canvas object

--- boredgames.js
+++ boredgames.js
@@ -1,1 +1,16 @@
-//s
+document.addEventListener("DOMContentLoaded", function(event){
+    var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext('2d');
+    alert("Function executed");
+    ctx.fillStyle = "beige";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawRectangle(ctx);
+    
+});
+
+function drawRectangle(ctx){
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(30, 30, 50, 50);
+}
+
+
