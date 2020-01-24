a21d3856f590788115b41ef97b9e1c791fd22b5c
paul117430404 <55983538+paul117430404@users.noreply.github.com>
2020-01-24 21:34:00 +0000

    Add files via upload
    added class for tile slots.

--- boredgames.js
+++ boredgames.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event){
     var canvas = document.getElementById('game_canvas');
     var ctx = canvas.getContext('2d');
-    alert("Function executed");
     ctx.fillStyle = "beige";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    var tile1 = 
+    var tile1 = new TileSlot(50, 50);
+    spawnTileSlot(ctx, "red", tile1, 10, 10);
     
 });
 
@@ -13,9 +13,26 @@ function drawRectangle(ctx){
     ctx.fillRect(30, 30, 50, 50);
 }
 
-function spawnTileSlot(ctx, tile_slot, tile_x, tile_y){
-    ctx.fillStyle = "green";
-    ctx.fillRect(tile_x, tile_y, tile_slot.x, tile_slot.y);
+function spawnTileSlot(ctx, style, tile_slot, tile_x_pos, tile_y_pos){
+    ctx.fillStyle = style;
+    tile_slot.drawSelf(ctx, tile_x_pos, tile_y_pos);
 }
 
-var tile_slot = {width: 50, height: 50, x: 0, y: 0};
+var basic_tile_slot = {width: 50, height: 50, x: 0, y: 0};
+
+class TileSlot{
+    
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+    
+    announceConstruction(){
+        alert("Construction complete");
+    }
+    
+    drawSelf(ctx, x_pos, y_pos){
+        ctx.fillRect(x_pos, y_pos, this.width, this.height);
+    }
+       
+}
