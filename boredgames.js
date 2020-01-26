8bab6f98bf4c31d59fe707a5511404a89623e285
pmg4 <117430404@umail.ucc.ie>
2020-01-26 18:10:37 +0000

    added spawn mechanics to lay out all tile slot objects onto the canvas

--- boredgames.js
+++ boredgames.js
@@ -3,8 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event){
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = "beige";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    var tile1 = new TileSlot(50, 50);
-    spawnTileSlot(ctx, "red", tile1, 10, 10);
+    var tile_slot_list = spawnAllTiles(ctx);
     
 });
 
@@ -18,7 +17,26 @@ function spawnTileSlot(ctx, style, tile_slot, tile_x_pos, tile_y_pos){
     tile_slot.drawSelf(ctx, tile_x_pos, tile_y_pos);
 }
 
+function spawnAllTiles(ctx){
+    var tile_slot_list = [];
+    var tile_slot_list_index = 0;
+    var spawn_point_x = 10;
+    var spawn_point_y = 10;
+    for (i = 0; i <10; i++){
+        for (j = 0; j < 10; j++){
+            tile_slot_list.push(new TileSlot(50, 50));
+            spawnTileSlot(ctx, "red", tile_slot_list[tile_slot_list_index], spawn_point_x, spawn_point_y);
+            tile_slot_list_index++;
+            spawn_point_x += 60;
+        }
+        spawn_point_x = 10;
+        spawn_point_y += 60;
+    }
+}   
+        
+        
 var basic_tile_slot = {width: 50, height: 50, x: 0, y: 0};
+
 
 class TileSlot{
     
@@ -36,4 +54,4 @@ class TileSlot{
     }
        
 }
-//test commiting here 1708
+
