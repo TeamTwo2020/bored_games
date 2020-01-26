10265708e05b4ebd48ea0c78e99c55a1e9154eb4
pmg4 <117430404@umail.ucc.ie>
2020-01-26 19:23:12 +0000

    added a function to detect when a tile is clicked on, to be used for debugging later

--- boredgames.js
+++ boredgames.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", function(event){
     ctx.fillStyle = "beige";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     var tile_slot_list = spawnAllTiles(ctx);
+    canvas.addEventListener('mousedown', function(e) {
+        tileSlotClicked(canvas, e, tile_slot_list);
+    });
+    console.log("done waiting m8");
     
 });
 
@@ -26,13 +30,41 @@ function spawnAllTiles(ctx){
         for (j = 0; j < 10; j++){
             tile_slot_list.push(new TileSlot(50, 50));
             spawnTileSlot(ctx, "red", tile_slot_list[tile_slot_list_index], spawn_point_x, spawn_point_y);
+            tile_slot_list[tile_slot_list_index].x = spawn_point_x;
+            tile_slot_list[tile_slot_list_index].y = spawn_point_y;
             tile_slot_list_index++;
             spawn_point_x += 60;
         }
         spawn_point_x = 10;
         spawn_point_y += 60;
     }
-}   
+    
+    return tile_slot_list;
+}
+
+function tileSlotClicked(canvas, event, tile_slot_list){
+    var canvas_border = canvas.getBoundingClientRect();
+    var y = event.clientY - canvas_border.top;
+    var x = event.clientX - canvas_border.left;
+    
+    for (i = 0; i < tile_slot_list.length; i ++){
+        if (x >= tile_slot_list[i].x && x <= (tile_slot_list[i].x + tile_slot_list[i].width)  && y >= tile_slot_list[i].y && y <= (tile_slot_list[i].y + tile_slot_list[i].height)){
+            console.log("tile slot clicked. Tile " + (i+1));
+            break;
+        }
+        
+        else{
+            console.log("clicked elsewhere. " + x);
+        }
+    }
+}
+
+function getCursorPosition(canvas, event){
+    var rectangle = canvas.getBoundingClientRect();
+    var x = event.clientX - rectangle.left;
+    var y = event.clientY - rectangle.top;
+    console.log("x: " + x + "y: " + y);
+}
         
         
 var basic_tile_slot = {width: 50, height: 50, x: 0, y: 0};
@@ -43,6 +75,8 @@ class TileSlot{
     constructor(width, height){
         this.width = width;
         this.height = height;
+        this.x;
+        this.y;
     }
     
     announceConstruction(){
