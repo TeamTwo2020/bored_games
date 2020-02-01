8aadad242fefe7b7daa7445ed0a3f1d3c6da8d88
pmg4 <117430404@umail.ucc.ie>
2020-02-01 20:13:23 +0000

    changed movement system slightly

--- topdowngame.js
+++ topdowngame.js
@@ -8,26 +8,30 @@ function init(){
     //Instantiate the canvas and its context.
     var canvas = document.getElementById('game_canvas');
     var ctx = canvas.getContext('2d');
-    var vertical_speed;
+    var vertical_speed = 0;
+    var recorded_event;
     
-    document.addEventListener('keypress', function(event){
-        if(event.keyCode == 119){
+    recorded_event = document.addEventListener('keydown', function(event){
+        if(event.keyCode == 87){
             vertical_speed = 5;
+            return event;
         }
         
-        if(event.keyCode == 100){
+        if(event.keyCode == 68){
         
         }
         
-        if(event.keyCode == 97){
+        if(event.keyCode == 65){
             
         }
         
-        if(event.keyCode == 115){
+        if(event.keyCode == 83){
             vertical_speed = -5;
+            return event;
         }
-        console.log("pressinga a key lmao " + event.keyCode);
-        console.log("v speed is " + vertical_speed);
+        
+        console.log("key event is " + event.keyCode);
+        
     });
     
     var falling_cube = {
@@ -42,21 +46,25 @@ function init(){
     //start the animations
     setInterval(function() {
         draw(canvas, ctx, falling_cube, player, vertical_speed);
+        
+        
     }, 10);
     
 }
 
 function draw(canvas, ctx, falling_cube, player, vertical_speed){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
     ctx.fillStyle = "beige";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "purple";
     ctx.fillRect(falling_cube.x, falling_cube.y, 50, 50);
-    falling_cube.y += 1;
-    falling_cube.x += 1;
+    falling_cube.y =falling_cube.y + vertical_speed;
+    //falling_cube.x += 1;
     
     
     ctx.fillRect(player.x, player.y, 50, 50);
     
     console.log("speed: " + vertical_speed);
+    //vertical_speed = 0;
 }
