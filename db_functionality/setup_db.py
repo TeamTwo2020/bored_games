9f57d3072ac860bdbec849d14871f8af19ecde9e
cgg1 <poop lol>
2020-02-03 12:21:29 +0000

    Removed error during previous commit

--- db_functionality/setup_db.py
+++ db_functionality/setup_db.py
@@ -2,13 +2,13 @@ def words_connect():
     import pymysql as db
     failed=0
 
-    server="cs1.ucc.ie"
-    database="2021_cgg1"
-    username="cgg1"
-    password="weeS2dih"
+    server="----"
+    database="----"
+    username="----"
+    password="----"
 
     try:
-        connection = db.connect('cs1.ucc.ie', 'cgg1', 'weeS2dih', '2021_cgg1')
+        connection = db.connect('----', '----', '----', '----')
         if connection:
             cursor =connection.cursor(db.cursors.DictCursor)
             if cursor:
