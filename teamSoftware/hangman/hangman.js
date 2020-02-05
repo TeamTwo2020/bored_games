03ce2de5abe2ef8c5bf399b363bb98e0b9e81c41
williamdoincollege <55444848+williamdoincollege@users.noreply.github.com>
2020-02-05 18:32:27 +0000

    added return statement to function comments()
    Does not stop player continuing after losing but no longer shows you win after initial you lose comment

--- teamSoftware/hangman/hangman.js
+++ teamSoftware/hangman/hangman.js
@@ -86,6 +86,7 @@ window.onload = function () {
     showLives.innerHTML = "You have " + lives + " lives";
     if (lives < 1) {
       showLives.innerHTML = "Game Over";
+      return;
     }
     for (var i = 0; i < geusses.length; i++) {
       if (counter + space === geusses.length) {
