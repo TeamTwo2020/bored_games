57233ad149016e809f1da61d87e69a3f7e983901
CristianGaudino <117434292@umail.ucc.ie>
2020-02-05 18:19:33 +0000

    Reverting issue with removed code

--- RectangleObject.js
+++ RectangleObject.js
@@ -8,4 +8,23 @@ class Rectangle {
         this.height=height;
         this.color=colour;
     }
+    
+    get bottom() { return this.y + this.height; }
+    get left(){ return this.x; }
+    get right(){ return this.x+this.width }
+    get top(){ return this.y }
+    
+    drawRect(ctx){
+        ctx.beginPath();
+        ctx.fillStyle=this.colour;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.closePath();
+    }
+    
+    testCollision(rectangle){
+        if (this.top > rectangle.bottom || this.right < rectangle.left || this.left >rectangle.right || this.bottom < rectangle.top ){
+            return false;
+        }
+        return true;
+    }
 }
