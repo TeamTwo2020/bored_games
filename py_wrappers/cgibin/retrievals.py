c9ac652bafd531bd88811246d3f766024559cb45
DidHeDed <116706819@umail.ucc.ie>
2020-02-20 23:31:21 +0000

    scoreboard retrieval code to call down table

--- py_wrappers/cgibin/retrievals.py
+++ py_wrappers/cgibin/retrievals.py
@@ -1,6 +1,36 @@
 #!/usr/local/bin/python3
 
-from cgitb import enable
-from py_wrappers.cgibin.connect import access_db as connect
+import pymysql as db
 
-enable()
+
+def get_scoreboard():
+
+    _acc_un, _acc_pw, _host, _dataB = "adc1", "quahgesh", "cs1.ucc.ie", "2020_adc1"
+    query = "SELECT 'USERNAME', 'SCORE' " \
+            "FROM 'players'" \
+            "ORDER BY 'SCORE' DESC;"
+    scores = ""
+    try:
+        # connect to database
+        connection = db.connect(_host, _acc_un, _acc_pw, _dataB)
+        # create cursor to navigate database
+        cursor = connection.cursor()
+        # Query database to retrieve scoreboard table
+        cursor.execute(query)
+        # output titled Scoreboard Table
+        scores += """<table>
+                    <tr><th>Player Top Scores</th><tr>"""
+        for row in cursor.fetchall():
+            # output username : score to table
+            scores += '<tr><td>%s : %i</td></tr>' % (row['USERNAME'], row['SCORE'])
+        # close off table
+        scores += '</table>'
+        # close the cursor
+        cursor.close()
+        # close the connection
+        connection.close()
+    except db.Error:
+        scores = '<p>Apologies, we are currently experiencing technical difficulties"</p>'
+        return scores
+
+
