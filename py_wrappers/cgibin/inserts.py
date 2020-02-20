494fbd1853aada6110c4ebf73d74d25536b8be49
DidHeDed <116706819@umail.ucc.ie>
2020-02-20 16:37:56 +0000

    connecting for statements that insert into the database. Had some issues with connections - will redress soon.

--- py_wrappers/cgibin/inserts.py
+++ py_wrappers/cgibin/inserts.py
@@ -1,9 +1,31 @@
 #!/usr/local/bin/python3
+import pymysql as db
 
-from cgitb import enable
-from py_wrappers.cgibin.connect import access_db as connect
 
-enable()
+def connect(typ, query):
+
+    _acc_un, _acc_pw, _host, _dataB = "adc1", "quahgesh", "cs1.ucc.ie", "2020_adc1"
+
+    connection = db.connect("cs1.ucc.ie", "adc1", "quahgesh", "2020_adc1")
+    cursor = connection.cursor()
+
+    if typ is "ins":
+        print("<p>Thank You! Details saved!</p>")
+        try:
+            cursor.execute(query)
+            connection.commit()
+        except db.Error:
+                print("<p>EXCEPTION_INSERT: FAILED, Authentication</p>")
+    elif typ is "del":
+        print("<p>:( Aww!</p>")
+        try:
+            cursor.execute(query)
+            connection.commit()
+        except db.Error:
+            print("<p>EXCEPTION_DELETE: FAILED</p>")
+            connection.rollback()
+    connection.close()
+    cursor.close()
 
 
 # Insertion query for new user to be added
@@ -13,27 +35,22 @@ Private function to Insert new member details to the database
 @param: string:username, string:password
 """
 
-def insert_new_user(username, password):
-    score, times_played = 0, 0
 
-    query = "INSERT INTO players('username', 'password', 'score', 'times_played') " \
-            "VALUES (%s, %s, %i, %i)", \
+def _insert_new_user(username, password):
+    typ, score, times_played = "ins", 0, 0
+
+    query = "INSERT INTO 'players' ('USERNAME', 'PASSWORD', 'SCORE', 'TIMES_PLAYED') " \
+            "VALUES ('%s', '%s', '%i', '%i')", \
             (username, password, score, times_played)
 
-    connect(query)
-    return
+    connect(typ, query)
 
 
-def insert_contact(fname, lname, email, country, message):
-    query = "INSERT INTO contact('fname', 'lname', 'email', 'country', 'message') " \
+def _insert_contact(fname, lname, email, country, message):
+    typ = "ins"
+
+    query = "INSERT INTO 'contact'('fname', 'lname', 'email', 'country', 'message') " \
             "VALUES (%s, %s, %s, %s, %s)", \
             (fname, lname, email, country, message)
-    connect(query)
+    connect(typ, query)
     return
-
-if __name__ == "__main__":
-    fname, lname, email, country, message = "alice", "smith", "a.smith@hotmail.com", "Cork", "TEST 1, 2"
-    username, password = "assmith", "d2sp"
-
-    insert_contact(fname, lname, email, country, message)
-    insert_new_user(username, password)
