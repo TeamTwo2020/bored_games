466eafa81f81b4f2c9924cf5eb2c4c0581c9e1cb
pmg4 <117430404@umail.ucc.ie>
2020-03-04 17:54:59 +0000

    Commented Room object and Wall object files

--- teamSoftware/topdown/RoomObject.js
+++ teamSoftware/topdown/RoomObject.js
@@ -1,27 +1,44 @@
+//The room object, which keeps track of all entities and objects within it
 class Room{
 	constructor(canvas, room_array, room_row_index, room_col_index, wall_thickness, left_door, right_door, upper_door, lower_door, room_color, wall_quantity){
-        console.log("wall quantity is " + wall_quantity);
+        
+        //The room needs to know which map or room array it is a part of
         this.room_array = room_array;
         this.canvas = canvas;
+        
+        //Is the player locked in. On by default.
         this.locked = true;
+        
+        //Door mats are the areas around each door. No walls can spawn there, to prevent the player getting trapped. You cannot see them by default.
         this.door_mats = [new Rectangle(wall_thickness, (canvas.height/2)-100, 200, 200, "lime"), new Rectangle(canvas.width - wall_thickness - 200, (canvas.height/2)-100, 200, 200, "lime"), new Rectangle((canvas.width/2) - 100, wall_thickness, 200, 200, "lime"), new Rectangle((canvas.width/2) - 100, (canvas.height - wall_thickness - 200), 200, 200, "lime")];
+        
+        //The room's neighbouring rooms. These denote where the player will end up if they pass through a door. Adjacent rooms do not count as a neighbour unless there is a door between them.
         this.right_neighbour;
         this.left_neighbour;
         this.upper_neighbour;
         this.lower_neighbour;
+        
+        //The room's position in the map
         this.room_row_index = room_row_index;
         this.room_col_index = room_col_index;
         this.wall_thickness = wall_thickness;
+        
+        //The border walls around the map. They may or may not have a gate.
 		this.left_wall = new Border(0, 0, wall_thickness, canvas.height, "black", left_door);
 		this.right_wall = new Border(canvas.width - wall_thickness, 0, wall_thickness, canvas.height, "black", right_door);
 		this.upper_wall = new Border(wall_thickness, 0, canvas.width - wall_thickness, wall_thickness, "black", upper_door);
 		this.lower_wall = new Border(wall_thickness, canvas.height - wall_thickness, canvas.width - wall_thickness, wall_thickness, "black", lower_door);
+        
+        //The room's colour
         this.color = room_color;
-        this.static_object_list = [];//wall
-        this.projectile_object_list = [];//bullet
-        this.entity_list = [];//hero
-        this.particle_list = [];
-        this.hero_list = [];
+        
+        //A series of lists, each tracking objects within the room.
+        this.static_object_list = [];//walls
+        this.projectile_object_list = [];//bullets
+        this.entity_list = [];//Henrys and stephen
+        this.particle_list = [];//Particle effects from bullet impacts
+        this.hero_list = [];//This is empty unless the hero is present in the room
+        
         this.wall_list = this.generateWalls(wall_quantity, canvas);
         for (var i = 0; i < this.wall_list.length; i++){
             //console.log("Item in wall list: " + i);
@@ -34,9 +51,9 @@ class Room{
 
 
     }
+    
+    //Generate enemies in the room. Takes in the hero, enemy number and a weapon value which determines the Henrys possible strength
     generateEnemies(hero, enemy_quantity, weapon_value){
-        /*for (var i = 0; i < enemy_quantity; i++{
-        }*/
         var henry_size = 50;
         var spawn_x;
         var spawn_y;
@@ -47,9 +64,13 @@ class Room{
             while (attempted_spawns < 50){
                 spawn_x = Math.random() * (this.canvas.width - (this.wall_thickness*2) - henry_size) + this.wall_thickness;
                 spawn_y = Math.random() * (this.canvas.height - (this.wall_thickness*2) - henry_size) + this.wall_thickness;
+                //If the weapon value is five, it is the final room. Stephen should spawn as a result.
                 if (weapon_value == 5){
                     new_henry = new Henry(spawn_x, spawn_y, henry_size, henry_size, "#ff9020", hero, this, 400, weapon_value);
-                } else {
+                } 
+                
+                //Otherwise, spawn a Henry
+                else {
                     new_henry = new Henry(spawn_x, spawn_y, henry_size, henry_size, "#9c1000", hero, this, 50, Math.round(Math.random() * weapon_value) + 1);
                 }
                 if (!this.isOnDoorMat(new_henry) && !this.isOnWall(new_henry)){
@@ -67,8 +88,8 @@ class Room{
         }
     }
     
+    //Adds the border blocks at the edge of the map to the static object list, so that they can have collision
     addBorderBlocks(){
-        //add borders to static objects
         this.static_object_list.push(this.left_wall.first_block);
         this.static_object_list.push(this.left_wall.third_block);
         if (this.left_wall.gate_status != "open"){
@@ -94,10 +115,8 @@ class Room{
         }
     }
 
-
+    //Draw the room, which consists of drawing its entire contents (every item in the particle list, entity list etc.).
 	drawSelf(ctx){
-        //console.log("left wall null?" + this.left_wall + "\nRight wall null?" + this.right_wall);
-
         if (this.left_wall != null){
             this.left_wall.drawSelf(ctx);
         }
@@ -107,7 +126,6 @@ class Room{
         }
 
         if (this.upper_wall != null){
-            //console.log("drawing upper wall");
             this.upper_wall.drawSelf(ctx);
         }
 
@@ -125,12 +143,13 @@ class Room{
             //console.log("drawing static object");
         }
         
+        //When drawing entites, they should also be given a chance to act.
         for (var e = 0; e < this.entity_list.length; e++){
             this.entity_list[e].moveAi();
             this.entity_list[e].shoot();
             this.entity_list[e].drawSelf(ctx);
         }
-        //console.log("Drawing something...");
+        
         for (var k = 0; k < this.projectile_object_list.length; k++){
             if (this.projectile_object_list[k].stopped){
                 this.projectile_object_list.splice(k, 1);
@@ -149,7 +168,8 @@ class Room{
             }
         }
     }
-
+    
+    //Generate the walls in the room.
     generateWalls(amount_of_walls, canvas){
 	    function not_block_gate(x,y){
 	        if(y>=425&&y<=540&&x<=85) {
@@ -171,7 +191,7 @@ class Room{
 
         }
 
-
+        //Ensure that they do not block the player
         function not_block_entity(x,y, room){
             if(y>=165&&y<=250&&x<=750&&x>=525) {
                 return false;//avoid block henry
@@ -211,13 +231,10 @@ class Room{
                         walls.push(wall);
                         arrayx.push(new_x);
                         arrayy.push(new_y);
-                        //console.log("pushed wall number: " + i);
                         break;
-                    } else{
-                        //console.log("can't place line at " + new_x + "   " + new_y);
-                    }
+                    } 
                 attempted_spawns++;
-                //console.log("attempted spawn: " + attempted_spawns);
+                
             }
             
             attempted_spawns = 0;
@@ -227,100 +244,72 @@ class Room{
         return walls;
     }
     
+    //Check if an object would be on a door mat. This is used when spawning walls.
     isOnDoorMat(object){
-            //alert("horizontal wall detected");
             var test_wall = object;
             for (var i = 0; i < this.door_mats.length; i++){
-                //console.log("CHECKING DOORMAT. mat x: " + this.door_mats[i].x + " mat y is " + this.door_mats[i].y);
                 if ((test_wall.x < this.door_mats[i].x + this.door_mats[i].width && test_wall.x > this.door_mats[i].x) && (test_wall.y < this.door_mats[i].y + this.door_mats[i].height && test_wall.y > this.door_mats[i].y)){
-                    //alert("reeee");
-                    //console.log("attempted to place x at " + new_x);
                     return true;
                 }
                 
                 else if ((test_wall.x < this.door_mats[i].x + this.door_mats[i].width && test_wall.x > this.door_mats[i].x) && (test_wall.y + test_wall.height < this.door_mats[i].y + this.door_mats[i].height && test_wall.y + test_wall.height > this.door_mats[i].y)){
-                    //console.log("attempted to place x at " + new_x);
                     return true;
                 }
                 
                 else if ((test_wall.x + test_wall.width < this.door_mats[i].x + this.door_mats[i].width && test_wall.x + test_wall.width > this.door_mats[i].x) && (test_wall.y < this.door_mats[i].y + this.door_mats[i].height && test_wall.y > this.door_mats[i].y)){
-                    //console.log("attempted to place x at " + new_x);
                     return true;
                 }
                 
                 else if ((test_wall.x + test_wall.width < this.door_mats[i].x + this.door_mats[i].width && test_wall.x + test_wall.width > this.door_mats[i].x) && (test_wall.y + test_wall.height < this.door_mats[i].y + this.door_mats[i].height && test_wall.y + test_wall.height > this.door_mats[i].y)){
-                    //console.log("attempted to place x at " + new_x);
                     return true;
                 }
                     
             }
                     
-            //console.log("working...");
             return false;
         }
     
+    //Check if an object would spawn on a wall. This is used when spawning Henrys    
     isOnWall(object){
         for (var i = 0; i < this.wall_list.length; i++){
-                //console.log("CHECKING DOORMAT. mat x: " + this.wall_list[i].x + " mat y is " + this.wall_list[i].y);
                 if ((object.x < this.wall_list[i].x + this.wall_list[i].width + 20 && object.x > this.wall_list[i].x) && (object.y < this.wall_list[i].y + this.wall_list[i].height + 20 && object.y > this.wall_list[i].y)){
-                    //alert("reeee");
-                    //console.log("attempted to place x at " + object.x);
-                    //this.static_object_list.push(new Rectangle(object.x, object.y, 50, 50, "lime"));
                     return true;
                 }
                 
                 else if ((object.x < this.wall_list[i].x + this.wall_list[i].width + 20 && object.x > this.wall_list[i].x) && (object.y + object.height < this.wall_list[i].y + this.wall_list[i].height + 20 && object.y + object.height > this.wall_list[i].y)){
-                    //console.log("attempted to place x at " + object.x);
-                    //this.static_object_list.push(new Rectangle(object.x, object.y, 50, 50, "lime"));
-                    
                     return true;
                 }
                 
                 else if ((object.x + object.width < this.wall_list[i].x + this.wall_list[i].width + 20 && object.x + object.width > this.wall_list[i].x) && (object.y < this.wall_list[i].y + this.wall_list[i].height + 20 && object.y > this.wall_list[i].y)){
-                    //console.log("attempted to place x at " + object.x);
-                    //this.static_object_list.push(new Rectangle(object.x, object.y, 50, 50, "lime"));
-                    
                     return true;
                 }
                 
                 else if ((object.x + object.width < this.wall_list[i].x + this.wall_list[i].width + 20 && object.x + object.width > this.wall_list[i].x) && (object.y + object.height < this.wall_list[i].y + this.wall_list[i].height + 20 && object.y + object.height > this.wall_list[i].y)){
-                    //console.log("attempted to place x at " + object.x);
-                    //this.static_object_list.push(new Rectangle(object.x, object.y, 50, 50, "lime"));
-                    
                     return true;
                 }
                     
             }
                     
-            //console.log("working...");
             return false;
     }
-
-    assignNeighbour(direction, room){
-        if (direction == "upper"){
-            this.upper_neighbour = room;
-        } else if (direction == "lower"){
-            this.lower_neighbour = room;
-        } else if (direction == "left"){
-            this.left_neighbour = room;
-        } else if (direction == "right"){
-            this.right_neighbour = room;
-        }
-    }
-
+    
+    
+    //Add an entity to the room's entity list
     addEntity(entity){
         this.entity_list.push(entity);
     }
-
+    
+    //Add a projectile to the room's projectile list
     addProjectile(projectile){
-        //console.log("projectile added");
         this.projectile_object_list.push(projectile);
     }
-
+    
+    //Add a particle to the room's particle list
     addParticle(particle){
 	    this.particle_list.push(particle);
     }
-
+    
+    //Print out all projectiles and their y speed. Used for debugging.
     printProjectile(){
 	    for (var i=0;i<this.projectile_object_list.length;i++){
 	        console.log("List item at pos: ", i, "=== ", this.projectile_object_list[i].y_speed);
@@ -334,7 +323,6 @@ class Room{
             case "left":
                 var border_wall = this.left_wall;
                 this.left_neighbour = this.room_array.array[this.room_row_index][this.room_col_index - 1];
-                //console.log("left neighbour: " + this.left_neighbour);
                 break;
             case "right":
                 var border_wall = this.right_wall;
@@ -351,13 +339,15 @@ class Room{
                 break;
         }
         
+        //Once a gate is created, it should be locked by default
         this.lockGates(border_direction);
         
         
         
-        //console.log("+++++++++finished iterating");
+        
     }
     
+    //When given a direction, the appropriate border wall will have its gate locked.
     lockGates(border_direction){
         var border_walls = [this.left_wall, this.right_wall, this.upper_wall, this.lower_wall];
         switch(border_direction){
@@ -379,38 +369,27 @@ class Room{
         }
         
         for (var i = 0; i < this.static_object_list.length; i++){
-            //console.log("iteration: " + i);
             if (this.static_object_list[i] == border_wall.second_block){
-                //console.log("Found second block, its status is " + border_wall.gate_status);
                 border_wall.second_block.color = "red";
                 border_wall.gate_status = "locked";
-                //console.log("second block status is now " + border_wall.gate_status);
+                
             }
-            
-            else{
-                //console.log("haven't found second block yet");
-            }
+                        
         }
         
     }
     
+    //When this is called, all locked gates will be opened, and the player will be allowed to travel through them again.
     openGates(){
         var border_walls = [this.upper_wall, this.right_wall, this.lower_wall, this.left_wall];
         for (var j = 0; j < border_walls.length + 1; j++){
             if (border_walls[j] != null){
                 for (var i = 0; i < this.static_object_list.length + 1; i++){
-                    //console.log("iteration: " + i);
                     if (this.static_object_list[i] == border_walls[j].second_block && border_walls[j].gate_status === "locked"){
-                        //console.log("Found second block, its status is " + border_wall.gate_status);
                         this.static_object_list.splice(i, 1);
                         border_walls[j].second_block.color = this.color;
                         border_walls[j].gate_status = "open";
                         console.log("opened gate");
-                        //console.log("second block status is now " + border_wall.gate_status);
-                    }
-                    
-                    else{
-                        //console.log("haven't found second block yet");
                     }
                 }
             }
--- teamSoftware/topdown/WallObject.js
+++ teamSoftware/topdown/WallObject.js
@@ -1,23 +1,25 @@
+//This is the class which makes the walls that populate rooms
 class Wall{
     constructor(x, y, preset_wall, color){
         this.x = x;
         this.y = y;
         this.width;
         this.height;
-      //  this.spawn_padding_width = 150;  my comment
-       // this.spawn_padding_height = 150;  my comment
+        //Contains the component blocks of the wall
         this.wall_blocks= [];
         this.color = color;
 
-        
         this.block_thickness = 35;
         this.populateWallBlocks(preset_wall);
     }
     
+    
+    //This method creates one of the preset wall designs and puts it in random coordinates in the room
     populateWallBlocks(preset_wall){
         this.wall_blocks.push(new Rectangle(this.x, this.y, this.block_thickness, this.block_thickness, this.color));
         
         switch(preset_wall){
+            //Case one is the horizontal wall preset
             case 0:
                 for (var i = 1; i < 5; i++){
                     this.wall_blocks.push(new Rectangle(this.wall_blocks[i-1].x+this.block_thickness, this.y, this.block_thickness, this.block_thickness, this.color));
@@ -25,15 +27,15 @@ class Wall{
                 
                 break;
             
+            //Case two is the vertical wall preset
             case 1:
                 for (var i = 1; i < 5; i++){
                     this.wall_blocks.push(new Rectangle(this.x,this.wall_blocks[i-1].y+this.block_thickness,  this.block_thickness, this.block_thickness, this.color));
                 }
                 
-                //this.width = this.
-                //console.log("case 1");
                 break;
                 
+            //Additional cases, currently not used
             case 2:
                 for (var i = 1; i < 5; i++){
                     this.wall_blocks.push(new Rectangle(this.wall_blocks[i-1].x+this.block_thickness,this.wall_blocks[i-1].y+this.block_thickness,  this.block_thickness, this.block_thickness, this.color));
@@ -74,18 +76,13 @@ class Wall{
             this.height = (furthest_down_block.y + this.block_thickness) - this.wall_blocks[0].y;
             this.width = (furthest_right_block.x + this.block_thickness) - this.wall_blocks[0].x;
             
-            //console.log("height is: " + this.height);
-            //console.log("width is: " + this.width);
-            
             
             
     }
     
-    
+    //Draw the wall, which consists of actually drawing each of its component blocks
     drawSelf(ctx){
-        //console.log("number of blocks: " + this.wall_blocks.length);
         for (var i = 0; i < this.wall_blocks.length; i++){
-            //console.log("Wb index: " + i + "wall blocks length: " + this.wall_blocks.length);
             this.wall_blocks[i].drawSelf(ctx);
             
         }
