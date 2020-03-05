181740fffd27ce93e650613ecce93aeb03947cc4
DidHeDed <116706819@umail.ucc.ie>
2020-03-05 13:49:08 +0000

    Committing database connection configuration file, php: Backend

---
+++ teamSoftware/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('dbhost', 'cs1.ucc.ie');
+    define('dbuser', 'cgg1');
+    define('dbpass', 'weeS2dih');
+    define('db', '2021_cgg1');
+    $db = mysqli_connect(dbhost, dbuser, dbpass, db);
+?>
